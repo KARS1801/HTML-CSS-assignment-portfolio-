9e7bb972eb9a9fa259f87728c2656f361a821dfb
Karan Shrivastav <kara.dhara@gmail.com>
2017-09-09 20:03:17 +0530

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,54 @@
+function changefb(){
+	                document.getElementById("fb").src="fbhover.jpeg";
+}
+
+function changew(){
+	                document.getElementById("w").src="whatsapphover.jpeg";
+}
+
+function changeg(){
+	                document.getElementById("g").src="githubhover.png";
+}
+
+function changem(){
+	                document.getElementById("m").src="gmailhover.jpeg";
+}
+
+
+
+
+function backfb(){
+	               document.getElementById("fb").src="fb.png";
+}
+
+function backw(){
+	               document.getElementById("w").src="whatsapp.png";
+}
+
+function backg(){
+	               document.getElementById("g").src="github.jpeg";
+}
+
+function backm(){
+	               document.getElementById("m").src="gmail.png";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
